dd000262b4db44a274eb323dc78c1dc194a47669
Alessandro Greco <alessandro.greco@uni-tuebingen.de>
2019-10-18 15:36:08 +0200

    Add __init__ file.

---
+++ mlreflect/__init__.py
@@ -0,0 +1,7 @@
+from mlreflect.reflectivity import multilayer_reflectivity
+from mlreflect.data_generator import TrainingData
+
+__all__ = ['multilayer_reflectivity', 'TrainingData']
+
+__author__ = "Alessandro Greco <alessandro.greco@uni-tuebingen.de>"
+__credits__ = ["Vladimir Starostin", "Christos Karapanagiotis", "Linus Pithan", "Stefan Kowarik"]
