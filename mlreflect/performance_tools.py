9a842ec716874ec279e07f7fd697e570a20176a5
Alessandro Greco <alessandro.greco@uni-tuebingen.de>
2019-10-23 19:37:17 +0200

    Add performance_tools module.

---
+++ mlreflect/performance_tools.py
@@ -0,0 +1,17 @@
+import functools
+from timeit import default_timer
+
+
+def timer(func):
+    """Print the runtime of the decorated function"""
+
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = default_timer()  # 1
+        value = func(*args, **kwargs)
+        end_time = default_timer()  # 2
+        run_time = end_time - start_time  # 3
+        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        return value
+
+    return wrapper_timer
