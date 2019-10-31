a95afc0abbbb9fe7be83e5108970a47d98b07ce1
Alessandro Greco <alessandro.greco@uni-tuebingen.de>
2019-10-31 09:02:19 +0100

    Add h5_tools to __init__.py

--- mlreflect/__init__.py
+++ mlreflect/__init__.py
@@ -2,8 +2,10 @@ from mlreflect.reflectivity import multilayer_reflectivity
 from mlreflect.data_generator import ReflectivityGenerator
 from mlreflect.preprocessing import InputPreprocessor, OutputPreprocessor
 from mlreflect.prediction import Prediction
+from mlreflect import h5_tools
 
-__all__ = ['multilayer_reflectivity', 'ReflectivityGenerator', 'InputPreprocessor', 'OutputPreprocessor', 'Prediction']
+__all__ = ['multilayer_reflectivity', 'ReflectivityGenerator', 'InputPreprocessor', 'OutputPreprocessor',
+           'Prediction', 'h5_tools']
 
 __author__ = "Alessandro Greco <alessandro.greco@uni-tuebingen.de>"
 __credits__ = ["Vladimir Starostin", "Christos Karapanagiotis", "Linus Pithan", "Stefan Kowarik"]
