b5ae6daca23a9520370ae97b5b8d9acf4f89c2df
Alessandro Greco <alessandro.greco@uni-tuebingen.de>
2019-10-31 10:08:29 +0100

    Fix recursive folder creation.

--- mlreflect/models/model_helpers.py
+++ mlreflect/models/model_helpers.py
@@ -6,11 +6,11 @@ from keras import callbacks as cb
 from mlreflect import h5_tools
 
 
-def create_save_directory(model_name: str):
-    directory_name = h5_tools.strip_file_extension(model_name)
+def create_save_directory(folder_name: str):
+    directory_name = h5_tools.strip_file_extension(folder_name)
 
     if not os.path.exists(directory_name):
-        os.mkdir(directory_name)
+        os.makedirs(directory_name)
 
 
 def make_save_path(model_folder: str):
@@ -18,9 +18,9 @@ def make_save_path(model_folder: str):
 
     dirname = os.path.dirname(model_folder)
     model_name = os.path.basename(model_folder)
-    base_wo_ext = h5_tools.strip_file_extension(model_name)
+    model_name_wo_ext = h5_tools.strip_file_extension(model_name)
 
-    path = os.path.join(dirname, base_wo_ext, base_wo_ext + '_' + time_stamp + '.h5')
+    path = os.path.join(dirname, model_name_wo_ext, model_name_wo_ext + '_' + time_stamp + '.h5')
     return path
 
 
@@ -32,5 +32,7 @@ def make_tensorboard_callback(model_folder: str, graphs_subfolder: str = 'tb_gra
     model_name_wo_ext = h5_tools.strip_file_extension(model_name)
 
     logdir = os.path.join(dirname, model_name_wo_ext, graphs_subfolder, time_stamp)
+
+    create_save_directory(logdir)
 
     return cb.TensorBoard(log_dir=logdir, histogram_freq=0, write_graph=True, write_images=True)
