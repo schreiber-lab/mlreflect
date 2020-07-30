68ffcebc02aed1ea1a8c5350ed2990fb1217e491
Alessandro Greco <alessandro.greco@uni-tuebingen.de>
2020-07-30 17:32:16 +0200

    Update __init__

--- mlreflect/__init__.py
+++ mlreflect/__init__.py
@@ -2,14 +2,16 @@ from .data_generation import noise
 from .data_generation.data_generator import ReflectivityGenerator
 from .data_generation.layers import Layer, MultilayerStructure
 from .data_generation.reflectivity import multilayer_reflectivity
+from .training.noise_generator import NoiseGenerator
 from .training.prediction import Prediction
 from .training.preprocessing import InputPreprocessor, OutputPreprocessor
 from .utils import h5_tools
+from .utils.check_gpu import check_gpu
 
-__version__ = '0.13.1'
+__version__ = '0.14.0'
 
 __all__ = ['multilayer_reflectivity', 'Layer', 'MultilayerStructure', 'ReflectivityGenerator', 'InputPreprocessor',
-           'OutputPreprocessor', 'Prediction', 'h5_tools', 'noise']
+           'OutputPreprocessor', 'NoiseGenerator', 'Prediction', 'h5_tools', 'noise', 'check_gpu']
 
 __author__ = "Alessandro Greco <alessandro.greco@uni-tuebingen.de>"
 __credits__ = ["Vladimir Starostin", "Christos Karapanagiotis", "Linus Pithan", "Stefan Kowarik"]
