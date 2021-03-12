f285b74ba64008fd6741d0ae007c7eded5eeeb1f
Alessandro Greco <alessandro.greco@uni-tuebingen.de>
2021-03-12 16:02:56 +0100

    Update test runner.

--- tests/runner.py
+++ tests/runner.py
@@ -3,12 +3,14 @@ import unittest
 import test_data_generator
 import test_layers
 import test_preprocessing
+import test_noise_generator
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
 
 suite.addTests(loader.loadTestsFromModule(test_data_generator))
 suite.addTests(loader.loadTestsFromModule(test_layers))
+suite.addTests(loader.loadTestsFromModule(test_noise_generator))
 suite.addTests(loader.loadTestsFromModule(test_preprocessing))
 
 runner = unittest.TextTestRunner(verbosity=3)
