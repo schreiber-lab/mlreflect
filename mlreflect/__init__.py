c5b31cebcb1aa1d339563f4d5af4d3535602ec3e
Alessandro Greco <alessandro.greco@uni-tuebingen.de>
2021-06-10 10:02:55 +0200

    Update version number.

--- mlreflect/__init__.py
+++ mlreflect/__init__.py
@@ -1,5 +1,4 @@
-__version__ = '0.16.0'
-
+__version__ = '0.17.0'
 
 __author__ = "Alessandro Greco <alessandro.greco@uni-tuebingen.de>"
 __credits__ = ["Vladimir Starostin", "Christos Karapanagiotis", "Linus Pithan", "Stefan Kowarik"]
