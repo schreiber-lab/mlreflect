a7ee107ad8b021b73e7dab38f55a28415cf1857e
Alessandro Greco <alessandro.greco@uni-tuebingen.de>
2021-06-10 10:00:49 +0200

    Update resource import.

--- mlreflect/models/trained_model.py
+++ mlreflect/models/trained_model.py
@@ -1,10 +1,8 @@
 import json
+from pathlib import Path
 
 import h5py
 import numpy as np
-import pkg_resources
-from importlib import resources
-from pathlib import Path
 from numpy import ndarray
 from tensorflow.keras import models
 from tensorflow.keras.models import Model
