928804034674c964790177d83a41c0220d301d3a
Alessandro Greco <alessandro.greco@uni-tuebingen.de>
2021-06-10 17:46:21 +0200

    Update example notebooks and add resource paths.

--- mlreflect/curve_fitter/__init__.py
+++ mlreflect/curve_fitter/__init__.py
@@ -1,4 +1,9 @@
+from pathlib import Path
+
 from .curve_fitter import CurveFitter
 from .spec_fitter import SpecFitter, DefaultSpecFitter
 
-__all__ = ['CurveFitter', 'SpecFitter', 'DefaultSpecFitter']
+example_spec_file_path = Path(__file__).parents[1] / Path('resources', 'example', 'example.spec')
+example_ascii_file_path = Path(__file__).parents[1] / Path('resources', 'example', 'example.dat')
+
+__all__ = ['CurveFitter', 'SpecFitter', 'DefaultSpecFitter', 'example_spec_file_path', 'example_ascii_file_path']
--- mlreflect/models/__init__.py
+++ mlreflect/models/__init__.py
@@ -1,5 +1,11 @@
+from pathlib import Path
+
 from . import model_helpers
 from . import simple_model
 from .trained_model import TrainedModel, DefaultTrainedModel
 
-__all__ = ['model_helpers', 'simple_model', 'TrainedModel', 'DefaultTrainedModel']
+default_model_path = Path(__file__).parents[1] / Path('resources', 'models', 'default_trained_model_with_absorption.h5')
+alternative_model_path = Path(__file__).parents[1] / Path('resources', 'models', 'different_model.h5')
+
+__all__ = ['model_helpers', 'simple_model', 'TrainedModel', 'DefaultTrainedModel', 'default_model_path',
+           'alternative_model_path']
