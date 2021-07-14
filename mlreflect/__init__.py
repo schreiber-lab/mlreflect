e8c75978ebb7c2a1b58f3b5d48f79b2e819434dc
Alessandro Greco <alessandro.greco@uni-tuebingen.de>
2021-07-14 17:10:37 +0200

    Unify version number.

--- mlreflect/__init__.py
+++ mlreflect/__init__.py
@@ -1,4 +1,6 @@
-__version__ = '0.17.2'
+from .version import __version__
+
+__version__ = __version__
 
 __author__ = "Alessandro Greco <alessandro.greco@uni-tuebingen.de>"
 __credits__ = ["Vladimir Starostin", "Christos Karapanagiotis", "Linus Pithan", "Stefan Kowarik"]
--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, find_packages
+from distutils.util import convert_path
+
+main_ns = {}
+with open(convert_path('mlreflect/version.py')) as ver_file:
+    exec(ver_file.read(), main_ns)
+
 
 with open('requirements.txt') as file:
     requirements = file.readlines()
 
 setup(
     name='mlreflect',
-    version='0.17.2',
+    version=main_ns['__version__'],
     author='Alessandro Greco',
     author_email='alessandro.greco@uni-tuebingen.de',
     license='GPL3',
