2ff8bc353bff0eaaca201433f0dedc599fdec957
Alessandro Greco <alessandro.greco@uni-tuebingen.de>
2021-11-10 17:17:21 +0100

    Add long description.

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 from distutils.util import convert_path
 
 from setuptools import setup, find_packages
@@ -9,12 +10,18 @@ with open(convert_path('mlreflect/version.py')) as ver_file:
 with open('requirements.txt') as file:
     requirements = file.readlines()
 
+
+def read(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
+
 setup(
     name='mlreflect',
     description='mlreflect is a Python package for training and using artificial neural networks to analyze '
                 'specular X-ray and neutron reflectivity data. The training and usage of the neural network models is '
                 'done via Keras as an API for TensorFlow.',
     version=main_ns['__version__'],
+    long_description=read('README.md'),
     author='Alessandro Greco',
     author_email='alessandro.greco@uni-tuebingen.de',
     license='MIT',
